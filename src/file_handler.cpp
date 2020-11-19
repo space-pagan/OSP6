9bfc375c1f1a0c2cfabc174dd197e360dfbade58
Zoya Samsonov <vsv28@mail.umsl.edu>
2020-11-19 03:44:47 -0600

    Same as prv

--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -33,8 +33,6 @@ void File::write(std::string msg) {
     this->stream = new std::fstream(this->name.c_str(), this->mode);
     // attempt to write a line to output file # filenum
     (*this->stream) << msg;
-    // if unsuccessful write, throw error
-    if (this->stream->bad()) customerrorquit("Unable to write to file");
     // flush line to file. Without this, line is only flushed when the file
     // is closed
     this->stream->flush();
